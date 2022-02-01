d07312fbdbbdb711716922516cc9c2fcd66b5436
Ivan Grigorev <82472551+Ivan-Grigorev@users.noreply.github.com>
2022-02-01 14:17:43 +0200

    Version 3

--- assistant_bot/urls.py
+++ assistant_bot/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import HomePage, AddressBookCreate, AddressBookView, delete_addressbook, AddressBookUpdate, AddressBookDetail, CustomLoginView, RegisterPage
 from django.contrib.auth.views import LogoutView
 
+from .views import HomePage, AddressBookCreate, AddressBookView, delete_addressbook, AddressBookUpdate, \
+    AddressBookDetail, CustomLoginView, RegisterPage, show_world_news, show_finance_news, show_sport_news, \
+    show_entertainment_news, show_hacker_news, show_weather
 
 url_patterns = [
     path('', HomePage.as_view(), name='homepage'),
@@ -12,5 +14,11 @@ url_patterns = [
     path('contact-view/<int:pk>', AddressBookDetail.as_view(), name='contact'),
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
-    path('register/', RegisterPage.as_view(), name='register')
+    path('register/', RegisterPage.as_view(), name='register'),
+    path('news/', show_world_news,  name='news'),
+    path('finance/', show_finance_news, name='finance'),
+    path('sport/', show_sport_news, name='sport'),
+    path('entertainment/', show_entertainment_news, name='entertainment'),
+    path('hacker/', show_hacker_news, name='hacker'),
+    path('weather/', show_weather, name='weather')
 ]
--- assistant_bot/views.py
+++ assistant_bot/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from .models import AddressBook
 from django.urls import reverse_lazy
 from django.db.models import Q
-# Create your views here.
 
 from django.views.generic import TemplateView, CreateView, ListView, UpdateView, DetailView
 
@@ -11,6 +10,16 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 from django.contrib import messages
+
+from newsapi import NewsApiClient
+
+import json
+
+import urllib.request
+
+from datetime import datetime
+
+from pycountry import countries
 
 
 class CustomLoginView(LoginView):
@@ -42,7 +51,7 @@ class RegisterPage(FormView):
     def form_invalid(self, form):
         messages.success(self.request, 'Your password is too common!')
         return super(RegisterPage, self).form_invalid(form)
-        
+
 
 class HomePage(TemplateView):
     template_name = 'homepage.html'
@@ -69,7 +78,8 @@ class AddressBookView(LoginRequiredMixin, ListView):
         search_input = self.request.GET.get('search-area')
         context['contacts'] = context['contacts'].filter(user=self.request.user)
         if search_input:
-            context['contacts'] = context['contacts'].filter(Q(name__startswith=search_input)|Q(surname__startswith=search_input))
+            context['contacts'] = context['contacts'].filter(
+                Q(name__startswith=search_input) | Q(surname__startswith=search_input))
 
         return context
 
@@ -93,3 +103,204 @@ class AddressBookDetail(LoginRequiredMixin, DetailView):
     model = AddressBook
     template_name = 'addressbook_detailview.html'
     context_object_name = 'contact'
+
+
+@login_required
+def show_world_news(request):
+    newsapi = NewsApiClient(api_key='92ee8fd958374204ada73045c7fe5936')
+    top = newsapi.get_top_headlines(sources='bbc-news')
+
+    content = top['articles']
+    desc = []
+    news = []
+    img = []
+
+    for i in range(len(content)):
+        text = content[i]
+        news.append(text['title'])
+        desc.append(text['description'])
+        img.append(text['urlToImage'])
+    mylist = zip(news, desc, img)
+
+    user_location = json.loads(urllib.request.urlopen('http://ipinfo.io/json').read())
+    weather_url = urllib.request.urlopen(
+        f"https://api.openweathermap.org/"
+        f"data/2.5/weather?q={user_location['city']}&appid=206c04ecd30711b37b3e460efd0e40d7"
+    ).read()
+    weather_data = json.loads(weather_url)
+    data = {
+        'name': weather_data['name'],
+        'temp_c': (weather_data['main']['temp'] - 273.15).__round__(1),
+        'temp_f': ((weather_data['main']['temp'] - 273.15) * 9 / 5 + 32).__round__(1)}
+
+    return render(request=request, template_name='news_page.html', context={'mylist': mylist, 'data': data})
+
+
+@login_required
+def show_finance_news(request):
+    newsapi = NewsApiClient(api_key='92ee8fd958374204ada73045c7fe5936')
+    top = newsapi.get_top_headlines(sources='business-insider')
+
+    content = top['articles']
+    desc = []
+    news = []
+    img = []
+
+    for i in range(len(content)):
+        text = content[i]
+        news.append(text['title'])
+        desc.append(text['description'])
+        img.append(text['urlToImage'])
+    mylist = zip(news, desc, img)
+
+    user_location = json.loads(urllib.request.urlopen('http://ipinfo.io/json').read())
+    weather_url = urllib.request.urlopen(
+        f"https://api.openweathermap.org/"
+        f"data/2.5/weather?q={user_location['city']}&appid=206c04ecd30711b37b3e460efd0e40d7"
+    ).read()
+    weather_data = json.loads(weather_url)
+    data = {
+        'name': weather_data['name'],
+        'temp_c': (weather_data['main']['temp'] - 273.15).__round__(1),
+        'temp_f': ((weather_data['main']['temp'] - 273.15) * 9 / 5 + 32).__round__(1)}
+
+    return render(request=request, template_name='finance_page.html', context={'mylist': mylist, 'data': data})
+
+
+@login_required()
+def show_sport_news(request):
+    newsapi = NewsApiClient(api_key='92ee8fd958374204ada73045c7fe5936')
+    top = newsapi.get_top_headlines(sources='bbc-sport')
+
+    content = top['articles']
+    desc = []
+    news = []
+    img = []
+
+    for i in range(len(content)):
+        text = content[i]
+        news.append(text['title'])
+        desc.append(text['description'])
+        img.append(text['urlToImage'])
+    mylist = zip(news, desc, img)
+
+    user_location = json.loads(urllib.request.urlopen('http://ipinfo.io/json').read())
+    weather_url = urllib.request.urlopen(
+        f"https://api.openweathermap.org/"
+        f"data/2.5/weather?q={user_location['city']}&appid=206c04ecd30711b37b3e460efd0e40d7"
+    ).read()
+    weather_data = json.loads(weather_url)
+    data = {
+        'name': weather_data['name'],
+        'temp_c': (weather_data['main']['temp'] - 273.15).__round__(1),
+        'temp_f': ((weather_data['main']['temp'] - 273.15) * 9 / 5 + 32).__round__(1)}
+
+    return render(request=request, template_name='sport_page.html', context={'mylist': mylist, 'data': data})
+
+
+@login_required()
+def show_entertainment_news(request):
+    newsapi = NewsApiClient(api_key='92ee8fd958374204ada73045c7fe5936')
+    top = newsapi.get_top_headlines(sources='buzzfeed')
+
+    content = top['articles']
+    desc = []
+    news = []
+    img = []
+
+    for i in range(len(content)):
+        text = content[i]
+        news.append(text['title'])
+        desc.append(text['description'])
+        img.append(text['urlToImage'])
+    mylist = zip(news, desc, img)
+
+    user_location = json.loads(urllib.request.urlopen('http://ipinfo.io/json').read())
+    weather_url = urllib.request.urlopen(
+        f"https://api.openweathermap.org/"
+        f"data/2.5/weather?q={user_location['city']}&appid=206c04ecd30711b37b3e460efd0e40d7"
+    ).read()
+    weather_data = json.loads(weather_url)
+    data = {
+        'name': weather_data['name'],
+        'temp_c': (weather_data['main']['temp'] - 273.15).__round__(1),
+        'temp_f': ((weather_data['main']['temp'] - 273.15) * 9 / 5 + 32).__round__(1)}
+
+    return render(request=request, template_name='entertainment_page.html', context={'mylist': mylist, 'data': data})
+
+
+@login_required
+def show_hacker_news(request):
+    newsapi = NewsApiClient(api_key='92ee8fd958374204ada73045c7fe5936')
+    top = newsapi.get_top_headlines(sources='hacker-news')
+
+    content = top['articles']
+    desc = []
+    news = []
+    img = []
+
+    for i in range(len(content)):
+        text = content[i]
+        news.append(text['title'])
+        desc.append(text['description'])
+        img.append(text['urlToImage'])
+    mylist = zip(news, desc, img)
+
+    user_location = json.loads(urllib.request.urlopen('http://ipinfo.io/json').read())
+    weather_url = urllib.request.urlopen(
+        f"https://api.openweathermap.org/"
+        f"data/2.5/weather?q={user_location['city']}&appid=206c04ecd30711b37b3e460efd0e40d7"
+    ).read()
+    weather_data = json.loads(weather_url)
+    data = {
+        'name': weather_data['name'],
+        'temp_c': (weather_data['main']['temp'] - 273.15).__round__(1),
+        'temp_f': ((weather_data['main']['temp'] - 273.15) * 9 / 5 + 32).__round__(1)}
+
+    return render(request=request, template_name='hacker_page.html', context={'mylist': mylist, 'data': data})
+
+
+@login_required()
+def show_weather(request):
+    user_location = json.loads(urllib.request.urlopen('http://ipinfo.io/json').read())
+    weather_url = urllib.request.urlopen(
+        f"https://api.openweathermap.org/"
+        f"data/2.5/weather?q={user_location['city']}&appid=206c04ecd30711b37b3e460efd0e40d7"
+    ).read()
+    weather_data = json.loads(weather_url)
+    data = {
+        'name': weather_data['name'],
+        'country': countries.get(alpha_2=weather_data['sys']['country']).name,
+        'temp_c': (weather_data['main']['temp'] - 273.15).__round__(1),
+        'temp_f': ((weather_data['main']['temp'] - 273.15) * 9 / 5 + 32).__round__(1),
+        'clouds': weather_data['clouds']['all'],
+        'wind_kmh': (weather_data['wind']['speed'] * 3.6).__round__(1),
+        'wind_mh': (weather_data['wind']['speed'] * 2.237).__round__(1),
+        'visibility_km': (weather_data['visibility'] / 1000).__round__(1),
+        'visibility_m': (weather_data['visibility'] / 1609).__round__(1),
+        'pressure': weather_data['main']['pressure'],
+        'humidity': weather_data['main']['humidity'],
+        'dt': datetime.fromtimestamp(weather_data['dt']).ctime()
+    }
+    if request.method == 'POST':
+        weather_url = urllib.request.urlopen(
+            f"https://api.openweathermap.org/"
+            f"data/2.5/weather?q={request.POST['city']}&appid=206c04ecd30711b37b3e460efd0e40d7"
+        ).read()
+        weather_data = json.loads(weather_url)
+        data = {
+            'name': weather_data['name'],
+            'country': countries.get(alpha_2=weather_data['sys']['country']).name,
+            'temp_c': (weather_data['main']['temp'] - 273.15).__round__(1),
+            'temp_f': ((weather_data['main']['temp'] - 273.15) * 9 / 5 + 32).__round__(1),
+            'clouds': weather_data['clouds']['all'],
+            'wind_kmh': (weather_data['wind']['speed'] * 3.6).__round__(1),
+            'wind_mh': (weather_data['wind']['speed'] * 2.237).__round__(1),
+            'visibility_km': (weather_data['visibility'] / 1000).__round__(1),
+            'visibility_m': (weather_data['visibility'] / 1609).__round__(1),
+            'pressure': weather_data['main']['pressure'],
+            'humidity': weather_data['main']['humidity'],
+            'dt': datetime.fromtimestamp(weather_data['dt']).ctime()
+        }
+        return render(request=request, template_name='result_weather_page.html', context={'data': data})
+    return render(request=request, template_name='weather_page.html', context={'data': data})
